8f2380894256a4bc51fc48de8fae9244fd61f0b9
Cliff <tzcliff@outlook.com>
2020-03-24 00:05:54 -0400

    add pages

--- StockTicker/app/src/main/java/com/cs591_mobile/stockticker/MainActivity.java
+++ StockTicker/app/src/main/java/com/cs591_mobile/stockticker/MainActivity.java
@@ -3,12 +3,44 @@ package com.cs591_mobile.stockticker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import io.flutter.app.FlutterFragmentActivity;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.FlutterEngineCache;
+import io.flutter.embedding.engine.dart.DartExecutor;
 
 public class MainActivity extends AppCompatActivity {
+    private FlutterEngine flutterEngine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        flutterEngine = new FlutterEngine(this);
+        // Start executing Dart code to pre-warm the FlutterEngine.
+        flutterEngine.getDartExecutor().executeDartEntrypoint(
+                DartExecutor.DartEntrypoint.createDefault()
+        );
+
+        // Cache the FlutterEngine to be used by FlutterActivity.
+        FlutterEngineCache
+                .getInstance()
+                .put("my_engine_id", flutterEngine);
+
+        startActivity(
+                FlutterActivity
+                        .withCachedEngine("my_engine_id")
+                        //.backgroundMode(FlutterActivity.BackgroundMode.transparent)
+                        .build(MainActivity.this));
+
+
+
+
+
+
     }
 }
