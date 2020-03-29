bb8906506b21bf9fe35f822ddc64f18ab5553100
Cliff <tzcliff@outlook.com>
2020-03-29 17:28:24 -0400

    fix registration

--- flutter_module/lib/screens/registration_screen.dart
+++ flutter_module/lib/screens/registration_screen.dart
@@ -70,7 +70,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                       showSpinner = true;
                     });
                     final newUser = await _auth.createUserWithEmailAndPassword(
-                        email: email, password: password);
+                        email: email.trim(), password: password);
                     if (newUser != null) {
                       Navigator.pushReplacementNamed(context, HomeScreen.id);
                       showSpinner = false;
