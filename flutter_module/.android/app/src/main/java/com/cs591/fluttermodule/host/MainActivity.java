bce7d6cfce4a9ddda846761ec9e61b2dd1d34006
Cedric Wille <cwille97@gmail.com>
2020-04-04 20:19:11 -0400

    fixed dependencies so that creating an account now works on my machine

--- flutter_module/.android/app/src/main/java/com/cs591/fluttermodule/host/MainActivity.java
+++ flutter_module/.android/app/src/main/java/com/cs591/fluttermodule/host/MainActivity.java
@@ -1,14 +1,7 @@
 package com.cs591.fluttermodule.host;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    GeneratedPluginRegistrant.registerWith(flutterEngine);
-  }
 }
